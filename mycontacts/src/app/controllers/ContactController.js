407360b2bce116d76afad2d77a2166fc293bb6be
Cassio Quintino <cassio.quintino@gmail.com>
2022-04-04 23:22:56 -0300

    add delete and store methods for ContactController

--- mycontacts/src/app/controllers/ContactController.js
+++ mycontacts/src/app/controllers/ContactController.js
@@ -8,20 +8,60 @@ class ContactController {
     response.json(contacts);
   }
 
-  show() {
+  async show(request, response) {
     // retorna um contato por id
+    const { id } = request.params;
+
+    const contact = await ContactRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
     // cria os contatos
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This email is already been taken' });
+    }
+
+    const contact = await ContactRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   update() {
     // atualiza um contato por id
   }
 
-  delete() {
+  async delete(request, response) {
     // deleta um contato por id
+    const { id } = request.params;
+
+    const contact = await ContactRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    await ContactRepository.delete(id);
+
+    // 204 - No Content
+    response.sendStatus(204);
   }
 }
 
--- mycontacts/src/app/repositories/ContactsRepository.js
+++ mycontacts/src/app/repositories/ContactsRepository.js
@@ -1,18 +1,61 @@
-const { uuid } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: uuidv4(),
     name: 'Cassio',
     email: 'teste@abc.com',
     phone: '1212121212',
-    category_id: uuid(),
+    category_id: uuidv4(),
+  },
+  {
+    id: uuidv4(),
+    name: 'Noah',
+    email: 'teste@abc.com',
+    phone: '1212121212',
+    category_id: uuidv4(),
   },
 ];
 
 class ContactRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.email === email),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+
+  create({
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: uuidv4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts.push(newContact);
+      resolve(newContact);
+    });
   }
 }
 
